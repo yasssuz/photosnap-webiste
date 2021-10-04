5f33f8bc06b3b12d5c59186409424666ac95034a
Karim Yassine <karimwebdevelopment@gmail.com>
2021-10-04 19:46:43 -0300

    Semantic correction

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,13 +3,13 @@ import Head from "next/head";
 
 const Home: NextPage = () => {
   return (
-    <div>
+    <main>
       <Head>
         <title>Photosnap | Home</title>
       </Head>
 
       <h1>hello</h1>
-    </div>
+    </main>
   );
 };
 
