1cb2c7a1ea634f2270d348d8db6a37b01f8ada84
Karim Yassine <karimwebdevelopment@gmail.com>
2021-10-04 19:47:10 -0300

    added globals styles and theming

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,42 @@
 import type { AppProps } from "next/app";
+import { createGlobalStyle, ThemeProvider } from "styled-components";
+import Navbar from "../components/shared/Navbar";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  const GlobalStyles = createGlobalStyle`
+    *,
+    *::before,
+    *::after {
+      margin: 0;
+      padding: 0;
+      box-sizing: border-box;
+    }
+
+    html {
+      font-size: 62.5%;
+
+      body, select, input, button {
+        font-family: 'DM Sans', sans-serif;
+      }
+    }
+  `;
+
+  const theme: object = {
+    colors: {
+      black: "#000000",
+      white: "#ffffff",
+      gray: "#DFDFDF",
+    },
+  };
+
+  return (
+    <>
+      <ThemeProvider theme={theme}>
+        <GlobalStyles />
+        <Navbar />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </>
+  );
 }
 export default MyApp;
