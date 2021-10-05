e5c05c6c45e8e20fb81e22da22eac72c47059eee
Karim Yassine <karimwebdevelopment@gmail.com>
2021-10-05 14:57:06 -0300

    added 3 banners

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import type { NextPage } from "next";
 import Head from "next/head";
+import Banner from "../components/shared/Banner";
 
 const Home: NextPage = () => {
   return (
@@ -8,7 +9,31 @@ const Home: NextPage = () => {
         <title>Photosnap | Home</title>
       </Head>
 
-      <h1>hello</h1>
+      <Banner
+        position='left'
+        height={65}
+        title='Create and share your photo stories. '
+        description='Photosnap is a platform for photographers and visual storytellers. We make it easy to share photos, tell stories and connect with others.'
+        hero={true}
+        image='/assets/home/desktop/create-and-share.jpg'
+        link='invite'
+      />
+      <Banner
+        position='right'
+        height={60}
+        title='BEAUTIFUL STORIES EVERY TIME'
+        description='We provide design templates to ensure your stories look terrific. Easily add photos, text, embed maps and media from other networks. Then share your story with everyone.'
+        image='/assets/home/desktop/beautiful-stories.jpg'
+        link='story'
+      />
+      <Banner
+        position='left'
+        height={60}
+        title='DESIGNED FOR EVERYONE'
+        description='Photosnap can help you create stories that resonate with your audience.  Our tool is designed for photographers of all levels, brands, businesses you name it.'
+        image='/assets/home/desktop/designed-for-everyone.jpg'
+        link='story'
+      />
     </main>
   );
 };
