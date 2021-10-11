df9747ede51f3b0a8f24987fd091102460717310
Karim Yassine <karimwebdevelopment@gmail.com>
2021-10-11 15:59:27 -0300

    ready  for deplay

---
+++ src/pages/features.tsx
@@ -0,0 +1,26 @@
+import type { NextPage } from "next";
+import Banner from "../components/shared/Banner";
+import Photographer from "../../public/assets/features/hero.jpg";
+import FeaturesList from "../components/shared/Features";
+import BetaInvite from "../components/shared/BetaInvite";
+
+const Features: NextPage = () => {
+  return (
+    <main>
+      <Banner
+        position='left'
+        title='FEATURES'
+        description='We make sure all of our features are designed to be loved by every aspiring and even professional photograpers who wanted to share their stories.'
+        image={Photographer.src}
+        blur={Photographer.blurDataURL}
+        hero={true}
+      />
+
+      <FeaturesList complete={true} />
+
+      <BetaInvite />
+    </main>
+  );
+};
+
+export default Features;
