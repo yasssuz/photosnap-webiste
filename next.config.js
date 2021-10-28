11a263965c6befe638de4bd8d3cb4e2e709de293
Karim Yassine <karimwebdevelopment@gmail.com>
2021-10-27 22:30:43 -0300

    added experimental features

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
+  experimental: {
+    concurrentFeatures: true,
+    serverComponents: true,
+  },
   images: {
     formats: ["image/avif", "image/webp"],
   },
